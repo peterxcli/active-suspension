1f184e8e9498c7cffcfa84e4de940a1ad4dbbd06
Peter Li <peterxcli@gmail.com>
2021-02-20 14:39:48 +0800

    sync

---
+++ env_obj.py
@@ -0,0 +1,11 @@
+class observation_space(object):
+    # self.shape = [4]
+    def __init__(self):
+        self.shape = [4] #
+        pass
+
+class action_space(object):
+    # self.shape = [1] #[e-current]
+    def __init__(self):
+        self.shape = [1] #[e-current]
+        pass
